e0c5b679aa0317c2a824c92d6e4f5c193c9236c4
athulpm <97athulpm@gmail.com>
2023-03-31 18:06:10 +0530

    Implemented sql table operation optimizations

--- server/controllers/premium.js
+++ server/controllers/premium.js
@@ -2,37 +2,23 @@ const { Sequelize } = require("sequelize");
 const Expense = require("../models/expense");
 const User = require("../models/user");
 
-exports.getLeaderboard = (req, res, next) => {
-  Expense.findAll({
-    attributes: ["userId", "amount", [Sequelize.col("user.name"), "name"]],
-    include: {
-      model: User,
-      attributes: [],
-    },
-  })
-    .then((result) => {
-      const expenses = result;
-      const userAmounts = {};
-
-      expenses.forEach((expense) => {
-        const name = expense.dataValues.name;
-        const amount = expense.dataValues.amount;
-
-        if (userAmounts[name]) {
-          userAmounts[name] += amount;
-        } else {
-          userAmounts[name] = amount;
-        }
-      });
-      const totalExpenses = Object.keys(userAmounts)
-        .map((name, index) => ({
-          id: index + 1,
-          name: name,
-          totalAmount: userAmounts[name],
-        }))
-        .sort((a, b) => b.totalAmount - a.totalAmount);
-      
-      res.status(200).json(totalExpenses);
-    })
-    .catch((err) => console.log(err));
+exports.getLeaderboard = async (req, res, next) => {
+  try {
+    const expenses = await Expense.findAll({
+      attributes: [
+        "userId",
+        [Sequelize.col("user.name"), "name"],
+        [Sequelize.fn("sum", Sequelize.col("amount")), "totalAmount"],
+      ],
+      group: ["userId"],
+      include: {
+        model: User,
+        attributes: [],
+      },
+      order: [['totalAmount', 'DESC']]
+    });
+    return res.status(200).json(expenses);
+  } catch (err) {
+    console.log(err);
+  }
 };
