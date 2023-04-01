2e1e641deb7b352648e0b49e016d65d4421ad81e
athulpm <97athulpm@gmail.com>
2023-04-01 11:16:03 +0530

    Implemented transactions to expense table updation

--- server/controllers/expense.js
+++ server/controllers/expense.js
@@ -1,5 +1,6 @@
 const Expense = require("../models/expense");
 const User = require("../models/user");
+const sequelize = require("../util/database");
 
 exports.getExpenses = async (req, res, next) => {
   const userId = req.user.dataValues.id;
@@ -12,32 +13,44 @@ exports.getExpenses = async (req, res, next) => {
 };
 
 exports.postAddExpense = async (req, res, next) => {
+  const t = await sequelize.transaction();
   const expenseData = req.body;
   try {
-    const result = await Expense.create({
-      ...expenseData,
-      userId: req.user.id,
-    });
+    const result = await Expense.create(
+      {
+        ...expenseData,
+        userId: req.user.id,
+      },
+      { transaction: t }
+    );
+    await t.commit();
     req.user.totalExpense = req.user.totalExpense + +expenseData.amount;
     await req.user.save();
     return res.status(201).json(result.dataValues);
   } catch (err) {
     console.log(err);
+    await t.rollback();
   }
 };
 
 exports.deleteExpense = async (req, res, next) => {
   const id = req.params.id;
+  const t = await sequelize.transaction();
   try {
-    const item = await Expense.findOne({
-      where: { id: id, userId: req.user.id },
-    });
+    const item = await Expense.findOne(
+      {
+        where: { id: id, userId: req.user.id },
+      },
+      { transaction: t }
+    );
     const amount = item.dataValues.amount;
     await item.destroy();
+    await t.commit();
     req.user.totalExpense = req.user.totalExpense - +amount;
     await req.user.save();
     return res.status(200).json("Deleted successfully");
   } catch (err) {
     console.log(err);
+    await t.rollback();
   }
 };
